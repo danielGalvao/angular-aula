cc42b13d5cd86f7b63bff985d7fcd243909a99f5
Daniel Galvão <galvaodev@gmail.com>
2016-09-11 15:51:17 -0300

    working with directives, expressions and controller

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,6 @@
+angular.module('alurapic').controller('FotosController',function($scope){
+  $scope.foto = {
+    titulo: 'Leão',
+    url: 'http://i0.wp.com/spacenews.com.br/wp-content/uploads/2015/11/maxresdefault.jpg?fit=2560%2C1600'
+  };
+});
