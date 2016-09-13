983f30dd437af2b4c7bc30c32e943a1a6a3772e9
Daniel Galvão <galvaodev@gmail.com>
2016-09-13 00:31:11 -0300

    Working with templateUrl to build my directive

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -6,13 +6,7 @@ angular.module('minhasDiretivas',[])
       titulo: '@'
     };
     ddo.transclude = true;
-    ddo.template =
-      '<div class="panel panel-default col-md-2">'
-    +'  <div class="panel-heading">'
-    +'    <h3 class="panel-title">{{titulo}}</h3>'
-    +'  </div>'
-    +'  <div class="panel-body" ng-transclude>'
-    +'  </div>'
-    +'</div>';
+    ddo.templateUrl = 'js/directives/meu-painel.html';
+
     return ddo;
   });
