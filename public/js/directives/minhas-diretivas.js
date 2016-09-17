d2a43ac609038e3ee56a811a73d464388fdc235f
Daniel Galvão <daniel.galvao@justdigital.com.br>
2016-09-16 23:11:20 -0300

    Add new directive to show img on form

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -9,4 +9,16 @@ angular.module('minhasDiretivas',[])
     ddo.templateUrl = 'js/directives/meu-painel.html';
 
     return ddo;
-  });
+  })
+  .directive('minhaFoto',function(){
+    var ddo = {};
+    ddo.restric = "AE";
+    ddo.scope = {
+      titulo: '@',
+      url: '@'
+    };
+    ddo.transclude = true;
+    ddo.templateUrl = 'js/directives/minha-foto.html';
+
+    return ddo;
+  })
