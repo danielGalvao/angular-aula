e3de11ac29a2cb3dee78f6ae20fa3c366294c238
Daniel Galvão <galvaodev@gmail.com>
2016-09-23 23:45:06 -0300

    Add factories

---
+++ public/js/services/meus-servicos.js
@@ -0,0 +1,36 @@
+angular.module('meusServicos', ['ngResource']).factory('recursoFoto',function($resource){
+return $resource('v1/fotos/:fotoId', null, {
+    update: {
+      method: 'PUT'
+    }
+  });
+})
+.factory('cadastroFotos', function(recursoFoto, $q){
+  var service = {};
+  service.cadastrar = function(foto) {
+    return $q(function(resolve, reject){
+      if(foto._id){
+        recursoFoto.update({fotoId: foto._id}, foto, function(){
+          resolve({
+            mensagem: 'Foto '+foto.titulo+' atualizada com sucesso!',
+            inclusao: false
+          });
+        }, function(erro){
+          console.log(erro);
+          reject('Não foi possível alterar a foto '+foto.titulo);
+        });
+      }else{
+        recursoFoto.save(foto, function(){
+          resolve({
+            mensagem: 'Foto '+foto.titulo+' incluída com sucesso!',
+            inclusao: true
+          });
+        }, function(erro){
+          console.log(erro);
+          reject('Não foi possível inserir a foto '+foto.titulo);
+        })
+      }
+    });
+  }
+  return service;
+});
